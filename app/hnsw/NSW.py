70c24b51640b7ce3ab61671182e2645c1b033680
Shogo <syougo19961223@gmail.com>
2024-10-27 16:45:57 +0900

    This is the temporary original HNSW algorithm

---
+++ app/hnsw/NSW.py
@@ -0,0 +1,27 @@
+#set the number of edge to have 
+#edge management
+    #find closest Node -> count -> if there is space ,add-> 
+
+class NSW:
+
+    def __init__(self,maximumFriend:int) -> None:
+        self.maximumFriend = maximumFriend
+        self.Nodes = []
+
+
+    def createGraph(self):
+
+        
+
+    def addNode(self,node):
+        self.Nodes.append(node)
+
+    def popNodeByID(self,id):
+        #traverse all of Node in Nodes and if found Node whose id is identical with argment yes delete it 
+
+    
+
+
+
+
+    #by initialize 
---
+++ app/hnsw/nodeCreator.py
@@ -0,0 +1,11 @@
+class Node:
+    def __init__(self,id,vector):
+        self.vector = vector 
+        self.id = id
+    
+
+    def getId(self):
+        return self.id
+    
+    def getVector(self):
+        return self.vector
